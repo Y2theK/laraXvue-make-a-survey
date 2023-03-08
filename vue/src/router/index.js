fd0a515fc66bceb441555537f554903e2d9fa5e2
Ye Yint Kyaw <yeyintkyaw300@gmail.com>
2023-03-08 21:39:29 +0630

    login, register , dashboard middleware

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,34 +1,55 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
 import Dashboard from "../views/Dashboard.vue";
 import Survey from "../views/Survey.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import store from "../store";
 const routes = [
     {
         path: "/",
         redirect: "/dashboard",
         name: "Dashboard",
         component: DefaultLayout,
+        meta: { requireAuth: true },
         children: [
             { path: "/dashboard", name: "Dashboard", component: Dashboard },
             { path: "/survey", name: "Survey", component: Survey },
         ],
     },
     {
-        path: "/login",
-        name: "Login",
-        component: Login,
-    },
-    {
-        path: "/register",
-        name: "Register",
-        component: Register,
+        path: "/auth",
+        redirect: "/login",
+        name: AuthLayout,
+        component: AuthLayout,
+        meta: { guestRoute: true },
+        children: [
+            {
+                path: "/login",
+                name: "Login",
+                component: Login,
+            },
+            {
+                path: "/register",
+                name: "Register",
+                component: Register,
+            },
+        ],
     },
 ];
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
-
+router.beforeEach((to, from, next) => {
+    if (to.meta.requireAuth && !store.state.user.token) {
+        next({ name: "Login" });
+    } else if (store.state.user.token && to.meta.guestRoute) {
+        next({ name: "Dashboard" });
+    } else {
+        next();
+    }
+});
 export default router;
